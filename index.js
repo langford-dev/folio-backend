521e4bb5473316db55406d84e55020673c1370dc
Langford Quarshie <langfordquarshie21@gmail.com>
2022-04-13 23:23:26 +0000

    added gradient

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 2003
 const userRoute = require('./routes/user.route')
+const meetingRoute = require('./routes/meeting.route')
 const connectToMongoDB = require('./config')
 const cors = require('cors')
 
@@ -10,6 +11,7 @@ connectToMongoDB()
 app.use(cors())
 app.use(express.json())
 app.use('/user', userRoute)
+app.use('/meeting', meetingRoute)
 
 app.get('/', (req, res) => { res.send('folio_server_online 🔥') })
 app.get('/favicon.ico', (req, res) => { res.send('/favicon.ico'); })
--- models/user.model.js
+++ models/user.model.js
@@ -7,97 +7,99 @@ const User = mongoose.model('User', Schema({
     id: {
         type: String,
         default: uuid.v4,
-        // required: [true, 'ID is required']
     },
 
     isPremiumAccount: {
         type: Boolean,
         default: false,
-        // required: [true, 'ID is required']
+    },
+
+    cv: {
+        type: String,
+        default: "",
     },
 
     profilePhoto: {
         type: String,
         default: '',
-        // required: [true, 'Profile photo is required']
     },
 
     coverPhoto: {
         type: String,
         default: 'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80',
-        // required: [true, 'Cover photo is required']
+    },
+
+    isGradient: {
+        type: Boolean,
+        default: false,
+    },
+
+    gradient: {
+        type: Object,
+        default: {
+            'from': "#07B5D4",
+            'from': "#3884F4",
+        }
     },
 
     themeColor: {
         type: String,
         default: "#ffffff",
-        // required: [true, 'Cover photo is required']
     },
 
     theme: {
         type: Number,
         default: 1,
-        // required: [true, 'Cover photo is required']
     },
 
     showGithubStats: {
         type: Boolean,
         default: false,
-        // required: [true, 'ShowGithubStats flag required']
     },
 
     fullname: {
         type: String,
         default: '',
-        // required: [true, 'Name is required']
     },
 
     email: {
         type: String,
         default: '',
-        // required: [true, 'Name is required']
     },
 
     username: {
         type: String,
         default: '',
-        // required: [true, 'Username is required']
     },
 
     socials: {
         type: Object,
         default: { twitter: "", dev: "", dailydev: "", hashnode: "", linkedin: "", stackoverflow: "", youtube: "", dribbble: "", twitch: "", discord: "", behance: "", instagram: "", github: "", coffee: "" },
-        // required: [true, 'Usernames are required']
     },
 
     work: {
         type: String,
         default: '',
-        // required: [true, 'Work is required']
     },
 
     views: {
         type: Number,
         default: 0,
-        // required: [true, 'Work is required']
     },
 
     clicks: {
         type: Number,
         default: 0,
-        // required: [true, 'Work is required']
     },
 
     tagline: {
         type: String,
         default: 'Hello world! 👋',
-        // required: [true, 'Work is required']
     },
 
     about: {
         type: String,
         default: 'Hello 👋 i just created my Folio profile',
-        // required: [true, 'About is required']
     },
 
     createdAt: {
@@ -113,19 +115,16 @@ const User = mongoose.model('User', Schema({
     workplaces: {
         type: Array,
         default: [],
-        // required: [true, 'Workplaces is required']
     },
 
     skills: {
         type: Array,
         default: [],
-        // required: [true, 'Skills are required']
     },
 
     projects: {
         type: Array,
         default: [],
-        // required: [true, 'Projects are required']
     },
 }));
 
