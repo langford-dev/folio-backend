28516095d76941f013e69e1a2a7b0e95f6867a7d
Langford Quarshie <langfordquarshie21@gmail.com>
2022-04-29 18:23:36 +0000

    removed dead code

--- models/user.model.js
+++ models/user.model.js
@@ -29,19 +29,6 @@ const User = mongoose.model('User', Schema({
         default: 'https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80',
     },
 
-    // isGradient: {
-    //     type: Boolean,
-    //     default: false,
-    // },
-
-    // gradient: {
-    //     type: Object,
-    //     default: {
-    //         'from': "#07B5D4",
-    //         'to': "#3884F4",
-    //     }
-    // },
-
     themeColor: {
         type: String,
         default: "#ffffff",
